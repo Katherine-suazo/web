5c159b24863cd3fa4afb268f2cbaea4abb0a7424
Katherine <katherinesuazo032@gmail.com>
2025-10-07 22:30:22 -0300

    Se agrego paginacion y se subieron datos a admin django

--- novedades/views.py
+++ novedades/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import Novedad
 
-# Create your views here.
+from django.core.paginator import Paginator
+
+# def nov(request):
+#     novedad = Novedad.objects.all()
+#     return render(request, "nov/novedades.html", {'novedad': novedad})
+
 
 def nov(request):
-    novedad = Novedad.objects.all()
+    noved = Paginator(Novedad.objects.all(), 2)
+    nov_number = request.GET.get("page")
+    novedad = noved.get_page(nov_number)
+
     return render(request, "nov/novedades.html", {'novedad': novedad})
